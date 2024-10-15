b7b069c098344ca10f2010e7afea1dd1cdcb7930
hhejo <litsmainjof@gmail.com>
2024-10-15 09:57:16 +0900

    feat: update backup

--- background-backup.js
+++ background-backup.js
@@ -1,40 +1,32 @@
 chrome.runtime.onInstalled.addListener(() => console.log('onInstalled'));
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log('onMessage:', message); // message: { action: 'setAlarms', morningTime: 'HH:mm', eveningTime: 'HH:mm' }
-  const { action, morningTime, eveningTime } = message;
+chrome.runtime.onMessage.addListener((message) => {
+  console.log('onMessage message:', message); // message: { action: 'setAlarms', morningTime: 'HH:mm', eveningTime: 'HH:mm' }
+  const { action, morningAlarmTime, eveningAlarmTime } = message;
   if (action === 'setAlarms') {
-    // const morningTime = message.morningTime;
-    // const eveningTime = message.eveningTime;
-    setAlarm(morningTime, 'morningTime');
-    setAlarm(eveningTime, 'eveningTime');
+    setAlarm(morningAlarmTime, 'morningAlarmTime');
+    setAlarm(eveningAlarmTime, 'eveningAlarmTime');
   }
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log('alarm:', alarm); // alarm: { name: 'morningAlarm', scheduledTime: 1728951240001.784 }
-  chrome.storage.sync.get(alarm.name, (result) => {
-    console.log('result:', result); // result: { morningTime: 'HH:mm' }
-    const alarmTime = result[alarm.name];
-    if (alarm.name === 'morningTime')
-      showNotification('입실 체크', `${alarmTime}`);
-    else if (alarm.name === 'eveningTime')
-      showNotification('퇴실 체크', `${alarmTime}`);
+  console.log('alarm:', alarm); // alarm: { name: 'morningAlarmTime', scheduledTime: 1728951240001.784 }
+  const { name: alarmName } = alarm;
+  chrome.storage.sync.get(alarmName, (result) => {
+    console.log('result:', result); // result: { morningAlarmTime: 'HH:mm' }
+    const alarmTime = result[alarmName];
+    if (alarmName === 'morningAlarmTime')
+      showNotification('입실 체크', `${alarmTime}입니다. 입실 체크하세요!!`);
+    else if (alarmName === 'eveningAlarmTime')
+      showNotification('퇴실 체크', `${alarmTime}입니다. 퇴실 체크하세요!!`);
   });
 });
 
-function setAlarm(timeString, alarmName) {
-  const [hours, minutes] = timeString.split(':').map(Number);
+function setAlarm(alarmTimeString, alarmName) {
   const now = new Date();
-  const alarmTime = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    hours,
-    minutes,
-    0,
-    0
-  );
+  const [hours, minutes] = alarmTimeString.split(':').map(Number);
+  const [year, mnth, date] = [now.getFullYear(), now.getMonth(), now.getDate()];
+  const alarmTime = new Date(year, mnth, date, hours, minutes, 0, 0);
   if (alarmTime < now) alarmTime.setDate(alarmTime.getDate() + 1);
   const timeUntilAlarm = alarmTime.getTime() - now.getTime();
   chrome.alarms.clear(alarmName, () => {
@@ -47,7 +39,7 @@ function showNotification(title, message) {
   chrome.notifications.create(
     {
       type: 'basic',
-      iconUrl: 'icon.png',
+      iconUrl: title === '입실 체크' ? 'icon1.png' : 'icon2.png',
       title: title,
       message: message,
       priority: 2,
--- popup-backup.js
+++ popup-backup.js
@@ -1,46 +1,29 @@
-document.querySelector('#saveTime').addEventListener('click', function () {
-  const morningTime = document.querySelector('#morningAlarmTime').value; // 'HH:mm'
-  const eveningTime = document.querySelector('#eveningAlarmTime').value; // 'HH:mm'
-  if (morningTime && eveningTime) {
-    chrome.storage.sync.set(
-      { morningTime: morningTime, eveningTime: eveningTime },
-      () => {
-        console.log('then! morningTime, eveningTime');
-        chrome.runtime.sendMessage({
-          action: 'setAlarms',
-          morningTime: morningTime,
-          eveningTime: eveningTime,
-        });
-      }
-    );
+document.querySelector('#saveTimes').addEventListener('click', function () {
+  const morningAlarmTime = document.querySelector('#morningAlarmTime').value; // 'HH:mm'
+  const eveningAlarmTime = document.querySelector('#eveningAlarmTime').value; // 'HH:mm'
+  if (morningAlarmTime && eveningAlarmTime) {
+    chrome.storage.sync.set({ morningAlarmTime, eveningAlarmTime }, () => {
+      chrome.runtime.sendMessage({
+        action: 'setAlarms',
+        morningAlarmTime: morningAlarmTime,
+        eveningAlarmTime: eveningAlarmTime,
+      });
+    });
   }
-  alert(`오전 알림 시간: ${morningTime}, 오후 알림 시간: ${eveningTime}`);
+  alert(`오전 알림: ${morningAlarmTime}\n오후 알림: ${eveningAlarmTime}`);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.sync.get(['morningTime', 'eveningTime'], (result) => {
-    if (result.morningTime)
-      document.querySelector('#morningAlarmTime').value = result.morningTime;
-    if (result.eveningTime)
-      document.querySelector('#eveningAlarmTime').value = result.eveningTime;
-  });
+  chrome.storage.sync.get(
+    ['morningAlarmTime', 'eveningAlarmTime'],
+    (result) => {
+      console.log('DOMContentLoaded result:', result); // result: { morningAlarmTime: 'HH:mm', eveningAlarmTime: 'HH:mm' }
+      if (result.morningAlarmTime)
+        document.querySelector('#morningAlarmTime').value =
+          result.morningAlarmTime;
+      if (result.eveningAlarmTime)
+        document.querySelector('#eveningAlarmTime').value =
+          result.eveningAlarmTime;
+    }
+  );
 });
-
-// document.querySelector('#saveTime').addEventListener('click', function () {
-//   const morningTime = document.querySelector('#morningAlarmTime').value; // 'HH:MM'
-//   const eveningTime = document.querySelector('#eveningAlarmTime').value; // 'HH:MM'
-//   if (morningTime && eveningTime) {
-//     chrome.storage.sync.set(
-//       { morningTime: morningTime, eveningTime: eveningTime },
-//       () => {
-//         console.log('morningTime, eveningTime:', morningTime, eveningTime);
-//         chrome.runtime.sendMessage({
-//           action: 'setAlarms',
-//           morningTime: morningTime,
-//           eveningTime: eveningTime,
-//         });
-//       }
-//     );
-//   }
-//   alert(`오전 알림 시간: ${morningTime}, 오후 알림 시간: ${eveningTime}`);
-// });
