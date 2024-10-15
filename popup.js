e44c9e8418d3a47d3b0e62a73f7c55ef0f146a96
hhejo <litsmainjof@gmail.com>
2024-10-15 09:38:53 +0900

    feat: update popup

--- popup.js
+++ popup.js
@@ -1,34 +1,29 @@
-document.querySelector('#saveTime').addEventListener('click', function () {
-  const morningTime = document.querySelector('#morningAlarmTime').value;
-  const eveningTime = document.querySelector('#eveningAlarmTime').value;
-
-  if (morningTime && eveningTime) {
-    chrome.storage.sync.set(
-      { morningTime: morningTime, eveningTime: eveningTime },
-      () => {
-        console.log('Alarm times:', morningTime, eveningTime);
-        chrome.runtime.sendMessage({
-          action: 'setAlarms',
-          morningTime: morningTime,
-          eveningTime: eveningTime,
-        });
-        // chrome.storage.local.get(console.log);
-        // setAlarm(morningAlarmTime, eveningAlarmTime);
-      }
-    );
+document.querySelector('#saveTimes').addEventListener('click', function () {
+  const morningAlarmTime = document.querySelector('#morningAlarmTime').value; // 'HH:mm'
+  const eveningAlarmTime = document.querySelector('#eveningAlarmTime').value; // 'HH:mm'
+  if (morningAlarmTime && eveningAlarmTime) {
+    chrome.storage.sync.set({ morningAlarmTime, eveningAlarmTime }, () => {
+      chrome.runtime.sendMessage({
+        action: 'setAlarms',
+        morningAlarmTime: morningAlarmTime,
+        eveningAlarmTime: eveningAlarmTime,
+      });
+    });
   }
-
-  alert(`알림 시간이 저장되었습니다! ${morningTime}, ${eveningTime}`);
-  // chrome.storage.local.get(console.log);
+  alert(`오전 알림: ${morningAlarmTime}\n오후 알림: ${eveningAlarmTime}`);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.sync.get(['morningTime', 'eveningTime'], (result) => {
-    if (result.morningTime) {
-      document.querySelector('#morningAlarmTime').value = result.morningTime;
+  chrome.storage.sync.get(
+    ['morningAlarmTime', 'eveningAlarmTime'],
+    (result) => {
+      console.log('DOMContentLoaded result:', result); // result: { morningAlarmTime: 'HH:mm', eveningAlarmTime: 'HH:mm' }
+      if (result.morningAlarmTime)
+        document.querySelector('#morningAlarmTime').value =
+          result.morningAlarmTime;
+      if (result.eveningAlarmTime)
+        document.querySelector('#eveningAlarmTime').value =
+          result.eveningAlarmTime;
     }
-    if (result.eveningTime) {
-      document.querySelector('#eveningAlarmTime').value = result.eveningTime;
-    }
-  });
+  );
 });
